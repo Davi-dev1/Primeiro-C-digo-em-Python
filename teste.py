6db6c5feb586b6f905f3374e0c0b73c3fe2dd468
Davi-dev1 <davigancalveesdasilva@gmail.com>
2025-08-26 15:25:37 -0300

    Primeiro código em Python da història da minha vida
    Signed-off-by: Davi-dev1 <davigancalveesdasilva@gmail.com>

---
+++ teste.py
@@ -0,0 +1,45 @@
+#Primeiro código em Python
+print(" Hello, World! ")
+
+##Operandos aritméticos relacionais e lógicos em python tudo diferente do que voce viu em java kkkkk
+
+#Python trabalha com 3 tipos de dados básicos: numérico, string e booleano.
+#Em Python não é necessário fazer uma declaração explícita das variáveis, não é necessário informar de antemão o tipo de dados ao qual ela pertence pois est definição ocorre pelo valor que ela armazena, a identificação é automática.
+
+
+#x = "Uninove"
+#y ="Nota do Aluno :"
+#z =10
+#a = False
+#p =5.2
+
+#print(type(x))
+#print(type(y))
+#print (type(z))
+#print(type(p))
+#print(x)
+#print("Instituição : ", x + ".", "Nota do Aluno :",z   )
+#print("Situação do Aluno : ", a)
+#print("Média do Aluno : ", p)
+#print(type(p))
+
+
+
+#print("Cadastro de Alunos")
+#nome = input("Dogite o nome do Aluno: ")
+#print("Aluno cadastrado com sucesso! Seja Bem Vindo(a) ", nome)
+#idade = input("Digite a idade do Aluno: ")
+#print("Idade do Aluno: ", idade)
+
+#x=23
+#y=-x
+#print("O valor de x era ", x ,"agora é ", y)
+
+a = 10
+b = 5   
+print("O resultado de a + b é ", a + b)
+
+d=16
+b=4
+print("O resultado de d / b é ", d / b)
+
